2d6da17ed4002011040868fc3847369e63b5f44d
Pavel Argentov <argentoff@gmail.com>
2017-12-26 13:05:47 +0300

    Removed all sequencing dsl from lwt calls

--- src/lib/operations.ml
+++ src/lib/operations.ml
@@ -23,12 +23,13 @@ let get_thermometer_value thermometer_file =
   let module T = Thermometer.Device(Thermometer.Linux) in
   let%lwt thermometer_value = T.read_temperature ~thermometer_file () in
   Lwt_log.info_f ~section "Got thermometer value as %.3f" thermometer_value
-  >> return thermometer_value
+  >>= fun () -> return thermometer_value
 
 let rec set_thermometer thermometer_file =
   Lwt_unix.sleep 5.0
-  >> get_thermometer_value thermometer_file >|= Prometheus.Gauge.set @@ Metrics.chip_temperature "soc_chip_temp"
-  >> set_thermometer thermometer_file
+  >>= fun () -> get_thermometer_value thermometer_file
+  >|= Prometheus.Gauge.set @@ Metrics.chip_temperature "soc_chip_temp"
+  >>= fun () -> set_thermometer thermometer_file
 
 let main thermometer_file prometheus_config =
   setup_logging ();
